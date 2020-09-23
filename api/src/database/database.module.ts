ae5e2dca1d6095f0277eaea893cf6f55784fcd1e
Jerry Wang <jerry@JerrydeMBP.hitronhub.home>
2020-09-22 18:17:05 -0700

    test remove ormconfig

--- api/src/database/database.module.ts
+++ api/src/database/database.module.ts
@@ -1,50 +1,50 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseService } from './database.service';
+// require('dotenv').config();
+// import { join } from 'path';
 
-import { join } from 'path';
-
-const entities =
-  process.env.NODE_ENV === 'development' ||
-  process.env.NODE_ENV === 'production'
-    ? join(__dirname, 'src/**/**.entity{.ts,.js}')
-    : 'dist/**/*.entity{ .ts,.js}';
-const synchronize =
-  process.env.NODE_ENV === 'development' ||
-  process.env.NODE_ENV === 'production'
-    ? false
-    : false;
-console.dir({
-  host: process.env.DB_HOST,
-  username: process.env.POSTGRESQL_USER,
-  password: process.env.POSTGRESQL_PASSWORD,
-  database:
-    process.env.NODE_ENV !== 'test'
-      ? process.env.POSTGRESQL_DATABASE
-      : process.env.DB_TEST_DATABASE,
-});
-const option: any = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: 5432,
-  username: process.env.POSTGRESQL_USER,
-  password: process.env.POSTGRESQL_PASSWORD,
-  database:
-    process.env.NODE_ENV !== 'test'
-      ? process.env.POSTGRESQL_DATABASE
-      : process.env.DB_TEST_DATABASE,
-  entities: [entities],
-  migrations: ['dist/migrations/*{.ts,.js}'],
-  cli: {
-    migrationsDir: 'src/migrations',
-  },
-  synchronize,
-  migrationsRun: process.env.NODE_ENV === 'production',
-  dropSchema: true,
-};
+// const entities =
+//   process.env.NODE_ENV === 'development' ||
+//   process.env.NODE_ENV === 'production'
+//     ? join(__dirname, 'src/**/**.entity{.ts,.js}')
+//     : 'dist/**/*.entity{ .ts,.js}';
+// const synchronize =
+//   process.env.NODE_ENV === 'development' ||
+//   process.env.NODE_ENV === 'production'
+//     ? false
+//     : false;
+// console.dir({
+//   host: process.env.DB_HOST,
+//   username: process.env.POSTGRESQL_USER,
+//   password: process.env.POSTGRESQL_PASSWORD,
+//   database:
+//     process.env.NODE_ENV !== 'test'
+//       ? process.env.POSTGRESQL_DATABASE
+//       : process.env.DB_TEST_DATABASE,
+// });
+// const option: any = {
+//   type: 'postgres',
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT, 10) || 5432,
+//   username: process.env.POSTGRESQL_USER,
+//   password: process.env.POSTGRESQL_PASSWORD,
+//   database:
+//     process.env.NODE_ENV !== 'test'
+//       ? process.env.POSTGRESQL_DATABASE
+//       : process.env.DB_TEST_DATABASE,
+//   entities: [entities],
+//   migrations: ['dist/migrations/*{.ts,.js}'],
+//   cli: {
+//     migrationsDir: 'src/migrations',
+//   },
+//   synchronize,
+//   migrationsRun: process.env.NODE_ENV === 'production',
+//   dropSchema: true,
+// };
 
 @Module({
-  imports: [TypeOrmModule.forRoot(option)],
+  imports: [TypeOrmModule.forRoot()],
   providers: [DatabaseService],
 })
 export class DatabaseModule {}
