6abd6d4c536840f4375fa9860c8faf72d1f512ee
fwpushan <pushan@freshworks.io>
2020-09-25 03:49:36 +0530

    Add terraform logic to setup aws instance for dev remote minishift

---
+++ devops/terraform/variable.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default = "ca-central-1"
+}
+
+variable "vpc_id" {
+    default = "vpc-06919e145a8827902"
+}
+
+variable "subnet_id" {
+    default = "subnet-07ab10543769589e0"
+}
