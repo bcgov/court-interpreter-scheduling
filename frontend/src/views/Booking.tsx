4cea4e59281cd3f315db111e3b8b1dbcfbf727c5
Michel Alexander <michel@freshworks.io>
2020-10-01 10:53:48 -0700

    get bookings from API

--- frontend/src/views/Booking.tsx
+++ frontend/src/views/Booking.tsx
@@ -4,17 +4,14 @@ import {
 } from '@material-ui/core'
 import BookingsTable from 'components/table/BookingsTable'
 import useAxios from 'axios-hooks'
-import DataService from 'data/index'
-
-const bookings = DataService.bookings(20)
 
 const Booking = () => {
   /* TODO add API call for GET booking */
-  /* const [{ data: bookings, error, loading }] = useAxios('/booking') */
+  const [{ data: bookings, error, loading }] = useAxios('/booking')
 
   return (
     <Box px='150px'>
-      <BookingsTable data={bookings} />
+      <BookingsTable data={bookings?.data} />
     </Box>
   )
 }
