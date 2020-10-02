f0f167592fcfb111543c5ca7ce02182c5997682e
Michel Alexander <michel@freshworks.io>
2020-10-02 14:41:58 -0700

    search components including the Range picker

---
+++ frontend/src/components/form/SearchDates.tsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect, useContext } from 'react'
+import {
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+  makeStyles,
+} from '@material-ui/core'
+import CloseIcon from '@material-ui/icons/Close'
+import moment from 'moment'
+
+import { Field, FieldArray, ArrayHelpers } from 'formik'
+
+import { StyledFormControl, StyledLabel, } from 'components/form/inputs/DirectoryInputs'
+import { ArrayPeriodCheckbox } from 'components/form/inputs/Check'
+import { BookingDate } from 'constants/interfaces'
+
+const useStyles = makeStyles({
+  parent: {
+    marginTop: '1rem',
+  },
+  closeIcon: {
+    float: 'right',
+  },
+});
+
+export default function SearchDates({ values }: { values: { dates: BookingDate[] }}) {
+  const classes = useStyles()
+  return (
+    <FieldArray
+      name='dates'
+      render={(arrayHelpers: ArrayHelpers) => (
+        <Grid wrap='nowrap' spacing={1} container className={classes.parent}>
+          {values.dates.map((bookinDate: BookingDate, index: number) => (
+            <Grid xs={2} item key={`fieldArray-dates-${index}`}>
+              <Card>
+                <CardContent>
+                  <CloseIcon className={`pointer ${classes.closeIcon}`} onClick={() => arrayHelpers.remove(index)} />
+                  <Typography variant='body1'>
+                    {moment(bookinDate.date).format('dddd')}
+                  </Typography>
+                  <Typography variant='h5' component='span' color='primary'>
+                    {moment(bookinDate.date).format('MMM D')}
+                  </Typography>
+                  <StyledFormControl>
+                    <StyledLabel htmlFor='time-options'>
+                      Arrival Time
+                    </StyledLabel>
+                    <Field type='time' name={`dates[${index}].arrival`} />
+                  </StyledFormControl>
+                  <div role='group' aria-labelledby='time-options'>
+                    <ArrayPeriodCheckbox
+                      index={index}
+                      label='Full Day'
+                      name={`dates[${index}].period`}
+                      fieldName='period'
+                      value='WHOLE_DAY'
+                    />
+                    <ArrayPeriodCheckbox
+                      index={index}
+                      label='Morning'
+                      name={`dates[${index}].period`}
+                      fieldName='period'
+                      value='MORNING'
+                    />
+                    <ArrayPeriodCheckbox
+                      index={index}
+                      label='Afternoon'
+                      name={`dates[${index}].period`}
+                      fieldName='period'
+                      value='AFTERNOON'
+                    />
+                  </div>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    />
+  )
+}
