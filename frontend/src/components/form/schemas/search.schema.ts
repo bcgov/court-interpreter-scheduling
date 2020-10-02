4d86d0491aded0fa49e8eb5c73a2e0896fb4b1d6
Michel Alexander <michel@freshworks.io>
2020-10-02 14:42:38 -0700

    update schema to use array of dates

--- frontend/src/components/form/schemas/search.schema.ts
+++ frontend/src/components/form/schemas/search.schema.ts
@@ -4,12 +4,18 @@ const Initial = {
   language: '',
   level: [],
   city: '',
+  dates: [],
 }
 
 const Schema = yup.object({
   language: yup.string(),
   level: yup.array().of(yup.string().matches(/(1|2|3|4)/)),
   city: yup.string(),
+  dates: yup.array().of(yup.object({
+    date: yup.string(),
+    arrival: yup.string(),
+    period: yup.array().of(yup.string().matches(/MORNING|AFTERNOON|WHOLE_DAY/))
+  }))
 })
 
 export { Schema, Initial }
--- frontend/src/constants/interfaces.ts
+++ frontend/src/constants/interfaces.ts
@@ -1,5 +1,18 @@
+export enum BookingPeriod {
+  MORNING = 'MORNING',
+  AFTERNOON = 'AFTERNOON',
+  WHOLE_DAY = 'WHOLE_DAY',
+}
+
+export type BookingDate = {
+  date: Date;
+  period?: BookingPeriod;
+  arrivalTime?: Date;
+}
+
 export type SearchParams = {
   language: string;
   level: string[];
   city: string;
+  dates: Array<BookingDate>
 }
