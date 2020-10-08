d55aa7fb59db92869ee4ef162d7471563310dcf0
Michel Alexander <michel@freshworks.io>
2020-10-08 13:26:46 -0700

    lazy load main app components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
-import React, { lazy } from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
-import Header from './components/layout/Header'
-import SubHeader from './components/layout/SubHeader'
-import ContentHeader from './components/layout/ContentHeader'
-import Footer from './components/layout/Footer'
+import { Box, CircularProgress } from '@material-ui/core'
+import Header from 'components/layout/Header'
+import SubHeader from 'components/layout/SubHeader'
+import ContentHeader from 'components/layout/ContentHeader'
+import Footer from 'components/layout/Footer'
 
-const Booking = lazy(() => import('./views/Booking'))
-const Directory = lazy(() => import('./views/Directory'))
+const Booking = lazy(() => import('views/Booking'))
+const Directory = lazy(() => import('views/Directory'))
 
 const App = () => {
   return (
@@ -15,11 +16,13 @@ const App = () => {
       <SubHeader />
       <section className='content'>
         <ContentHeader />
-        <Switch>
-          <Route exact path='/directory' component={Directory} />
-          <Route exact path='/booking' component={Booking} />
-          <Redirect to='/directory' />
-        </Switch>
+        <Suspense fallback={<Box p={2}><CircularProgress /></Box>}>
+          <Switch>
+            <Route exact path='/booking' component={Booking} />
+            <Route exact path='/directory' component={Directory} />
+            <Redirect to='/booking' />
+          </Switch>
+        </Suspense>
       </section>
       <Footer />
     </>
