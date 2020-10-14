091204a5a22ffab1226e6898e0ec8856fcd87e38
Jerry Wang <jerry@JerrydeMBP.hitronhub.home>
2020-10-13 23:03:57 -0700

    add sonar

--- api/src/sonar.ts
+++ api/src/sonar.ts
@@ -2,7 +2,7 @@ const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner(
   {
-    serverUrl: 'http://localhost:9000',
+    serverUrl: `http://${process.env.SONAR_HOST}:${process.env.SONAR_PORT}`,
     options: {
       'sonar.sources': '.',
       'sonar.inclusions': 'src/**',
