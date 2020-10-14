073d904fd56f70e251664f96e3620c151521c5c8
Michel Alexander <michel@freshworks.io>
2020-10-14 12:02:35 -0700

    update useError to handle situation where error.message is undefined

--- frontend/src/hooks/useError.ts
+++ frontend/src/hooks/useError.ts
@@ -9,10 +9,11 @@ export default function useError ({ error, prefix }: {
   const { updateErrorContext } = useContext(ErrorContext)
   useEffect(() => {
     if (error) {
+      error.message = error.message || 'An unknown error occurred.'
       if (error.response?.data?.message) {
         error.message = error.response?.data?.message
       }
-      const message = prefix ? `${prefix} ${error.message}` : `${error.message || 'An unknown error occurred'}`
+      const message = prefix ? `${prefix} ${error.message}` : error.message
       updateErrorContext(message)
     }
   }, [error])
