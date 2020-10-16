d0117b067649faf7fa71412172b8674d6e1b6267
Michel Alexander <michel@freshworks.io>
2020-10-16 12:51:26 -0700

    interpreter Type and schema

---
+++ frontend/src/components/form/schemas/interpreter.schema.ts
@@ -0,0 +1,74 @@
+import * as yup from 'yup'
+import { BookingType } from 'components/form/schemas/booking.schema'
+import { Language } from 'constants/interfaces'
+
+const Initial = {
+  firstName: '',
+  lastName: '',
+  language: [{
+    languageName: '',
+    level: '',
+    commentOnLevel: ''
+  }],
+  address: '',
+  city: '',
+  province: 'BC',
+  postal: '',
+  homePhone: '',
+  businessPhone: '',
+  phone: '',
+  email: '',
+  supplier: '',
+  gst: '',
+  comments: '',
+  contractExtension: false,
+  contractTermination: false,
+}
+
+const Schema = yup.object({
+  firstName: yup.string(),
+  lastName: yup.string(),
+  language: yup.array().of(yup.object({
+    name: yup.string(),
+    level: yup.string().matches(/(1|2|3|4)/, 'Level must be a number from 1-4'),
+    comment: yup.string(),
+  })),
+  address: yup.string(),
+  city: yup.string(),
+  province: yup.string(),
+  postal: yup.string(),
+  homePhone: yup.string(),
+  businessPhone: yup.string(),
+  phone: yup.string(),
+  email: yup.string(),
+  supplier: yup.string(),
+  gst: yup.string(),
+  comments: yup.string(),
+  contractExtension: yup.boolean(),
+  contractTermination: yup.boolean(),
+})
+
+export type Interpreter = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  language: Language[];
+  bookings: BookingType[];
+  address?: string;
+  city?: string;
+  province?: string;
+  postal?: string;
+  homePhone?: string;
+  businessPhone?: string;
+  phone?: string;
+  email?: string;
+  supplier?: string;
+  gst?: string;
+  comments?: string;
+  contractExtension?: boolean;
+  contractTermination?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export { Schema, Initial }
