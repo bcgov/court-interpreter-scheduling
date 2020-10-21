7308b15519c0475dca33f2a5be04139c29de4718
Jerry Wang <46575401+jerryfreshworks@users.noreply.github.com>
2020-10-21 15:36:06 -0700

    add keycloak (#43)
    * add keycloak
    
    * typo
    
    Co-authored-by: Jerry Wang <jerry@JerrydeMBP.localdomain>

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from 'nest-keycloak-connect';
+
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,13 +8,20 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('healthcheck')
+  @Public()
   healthCheck(): string {
     console.log('health-check log');
     return 'hello healthcheck';
+  }
+
+  @Get('keycloak')
+  keycloak() {
+    return 'key cloak success';
   }
 }
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -8,9 +8,10 @@ import { InterpreterModule } from './interpreter/interpreter.module';
 import { LanguageModule } from './language/language.module';
 import { LocationModule } from './location/location.module';
 import { BookingModule } from './booking/booking.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, InterpreterModule, LanguageModule, LocationModule, BookingModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, InterpreterModule, LanguageModule, LocationModule, BookingModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
