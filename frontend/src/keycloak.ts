4e9379df10d38a2eec0e82002be9cb7fdc995294
will-freshworks <70539895+will-freshworks@users.noreply.github.com>
2020-10-26 10:04:16 -0700

    New frontend/Dockerfile for OCP4 deployment (#54)
    * Updated Dockerfile and keycloak settings for OCP4
    * The latest docker-compose

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -2,7 +2,9 @@ import Keycloak from 'keycloak-js'
 
 const keycloak: Keycloak.KeycloakInstance = Keycloak({
   realm: 'court',
-  url: process.env.NODE_ENV === 'production' ? 'https://secure-keycloak-court-interpreter-scheduler.pathfinder.gov.bc.ca/auth' : 'http://127.0.0.1:9090/auth',
+  url: process.env.NODE_ENV === 'production'
+    ? 'https://court-interpreter-scheduling.apps.silver.devops.gov.bc.ca/auth'
+    : 'http://127.0.0.1:9090/auth',
   clientId: 'court-fe'
 })
 
