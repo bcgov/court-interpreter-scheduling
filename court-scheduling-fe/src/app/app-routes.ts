2d5d749a5fcbeef3f21a08a4877f44b1d60a7b3a
Jerry Wang <46575401+jerryfreshworks@users.noreply.github.com>
2020-11-13 11:34:36 -0800

    delete unused code and key (#81)
    Co-authored-by: Jerry Wang <jerry@JerrydeMBP.hitronhub.home>

--- court-scheduling-fe/src/app/app-routes.ts
+++
@@ -1,9 +0,0 @@
-export enum AppRoutes {
-    Error = 'error',
-    Root = '',
-    Login = 'login',
-    Bookings = 'bookings',
-    Interpreters = 'interpreters',
-    Admin = 'admin',
-    Clerks = 'clerks'
-}
