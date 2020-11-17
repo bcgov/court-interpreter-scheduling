4cffae0b3849ce383884d1d2b1189aba6d1b3104
fwpushan <pushan@freshworks.io>
2020-11-18 00:25:47 +0530

    Update devops templates with bc gov kc values

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js'
 
 const keycloak: Keycloak.KeycloakInstance = Keycloak({
-  realm: 'court',
+  realm: process.env.KC_REALM,
   url: process.env.REACT_APP_REALM_AUTH_URL,
   clientId: 'court-fe'
 })
