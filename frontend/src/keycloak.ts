39a00ae8c106bbed64cd07a31bd0a2a75d3d8091
Michel Alexander <michel@freshworks.io>
2020-11-30 12:00:34 -0800

    prefix KC realm with REACT_APP so FE build step can access it

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -1,8 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak: Keycloak.KeycloakInstance = Keycloak({
-  realm: localStorage.getItem('keycloakRealm') ||
-  process.env.KC_REALM || 'joxo7v8f',
+  realm: localStorage.getItem('keycloakRealm') || process.env.REACT_APP_KC_REALM || 'joxo7v8f',
   url: localStorage.getItem('keycloakAuthUrl') || process.env.REACT_APP_REALM_AUTH_URL,
   clientId: 'court-fe',
 });
