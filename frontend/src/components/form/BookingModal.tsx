8c405b44f7ff292b139b294c7e3986807c2220c4
will-freshworks <70539895+will-freshworks@users.noreply.github.com>
2020-12-03 17:38:07 -0800

    Fixed another couple of circular references (#24)

--- frontend/src/components/form/BookingModal.tsx
+++ frontend/src/components/form/BookingModal.tsx
@@ -47,7 +47,7 @@ export default function BookingModal({ interpreter, setInterpreter }: BookingMod
       setInterpreter(null)
       history.push('/booking')
     }
-  }, [response, setInterpreter, history])
+  }, [response])
 
   return (
     <SearchContext.Consumer>
