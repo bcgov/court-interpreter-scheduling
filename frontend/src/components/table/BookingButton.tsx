706387a659b8d6f3c50f0163643a8eb25580d5fd
Michel <60482131+michel-fresh@users.noreply.github.com>
2021-01-14 17:06:26 -0800

    City table sort (#36) (#37)
    * add a tooltip to disabled booking button
    
    * sort interpreter search table by city on address column, update locations list with coordinates
    
    * remove unused imports
    
    * - hide location fields
    - update dir search to use autocompleteInput for language search
    
    * return all languages from search interpreter endpoint
    
    * capitalize asl and cart in languages list
    
    * prefer LIKE to = in interpreter search language query
    
    * highlight searched language in results table based on includes as opposed to = as per API update

--- frontend/src/components/table/BookingButton.tsx
+++ frontend/src/components/table/BookingButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core'
 import Button, { ButtonProps } from '@material-ui/core/Button'
+import Tooltip from '@material-ui/core/Tooltip'
 
 export const StyledButton = withStyles({
   root: {
@@ -15,5 +16,9 @@ export const StyledButton = withStyles({
 })(Button)
 
 export default function BookingButton(props: ButtonProps) {
-  return <StyledButton {...props}>Book</StyledButton>
+  return props.disabled ? (
+    <Tooltip title='Please select dates to create a new booking'>
+      <span><StyledButton {...props}>Book</StyledButton></span>
+    </Tooltip>
+  ) : <StyledButton {...props}>Book</StyledButton>
 }
