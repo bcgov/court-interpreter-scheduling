c18c45e15ec95691ae3fa6af88e9b53a308708cd
Jerry Wang <jerry@freshworks.io>
2021-03-31 23:05:25 -0700

    add constraints

--- api/src/booking/entities/booking.entity.ts
+++ api/src/booking/entities/booking.entity.ts
@@ -64,7 +64,7 @@ export class BookingEntity {
   @ManyToOne(
     type => LanguageEntity,
     (language: LanguageEntity) => language.name,
-    { eager: true },
+    { eager: true, onDelete: 'SET NULL' },
   )
   language: LanguageEntity;
 
