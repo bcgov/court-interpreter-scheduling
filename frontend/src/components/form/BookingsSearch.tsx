0464ae0ade23da8abefb8703b80905e5a66a94d1
Pushan Mitra <46456190+fwpushan@users.noreply.github.com>
2021-04-24 01:56:01 +0530

    [api][frontend] Booking location filter (#115)
    ------------
    1. Add court location based search filter on booking
    2. Fix location assignement issue for create or update booking
    --

--- frontend/src/components/form/BookingsSearch.tsx
+++ frontend/src/components/form/BookingsSearch.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-import moment from 'moment'
+import moment from 'moment';
 
-import {
-  Box,
-  Grid,
-} from '@material-ui/core'
+import { Box, Grid } from '@material-ui/core';
 
 import {
   StyledFormControl,
@@ -13,94 +10,125 @@ import {
   StyledTextField,
   StyledFormLabel,
   GridRow,
-} from './inputs/DirectoryInputs'
+} from './inputs/DirectoryInputs';
 
-import { Schema, Initial } from 'components/form/schemas/booking-search.schema'
-import { ButtonPrimary } from 'components/Buttons'
-import BookingsRange from 'components/form/BookingsRange'
+import { Schema, Initial } from 'components/form/schemas/booking-search.schema';
+import { ButtonPrimary } from 'components/Buttons';
+import BookingsRange from 'components/form/BookingsRange';
 
-import { ErrorMessage, Field, Form, Formik, FormikProps } from 'formik'
+import { ErrorMessage, Field, Form, Formik, FormikProps } from 'formik';
+import { getLocationDetails } from 'util/locationFetch';
+import AutocompleteInput from './inputs/Autocomplete';
 
-const dateFormat = 'MMM D, YYYY'
+const dateFormat = 'MMM D, YYYY';
 
-export default function Search({ getSearchResults }: { getSearchResults: Function }) {
+export default function Search({
+  getSearchResults,
+}: {
+  getSearchResults: Function;
+}) {
+  const [locations, setLocations] = useState(['Loading...']);
+  useEffect(() => {
+    async function fetchLocation() {
+      const fetchedLocations: string[] = await getLocationDetails();
+      setLocations(fetchedLocations);
+    }
+    fetchLocation();
+  }, [setLocations]);
   return (
     <Box>
       <Formik
         initialValues={Initial}
         validationSchema={Schema}
-        onSubmit={async (values) => getSearchResults({
-          url: '/booking/search',
-          method: 'POST',
-          data: {
-            ...values,
-            dates: values?.dates?.startDate ? [values.dates] : [],
-          }
-        })}>
+        onSubmit={async (values) =>
+          getSearchResults({
+            url: '/booking/search',
+            method: 'POST',
+            data: {
+              ...values,
+              dates: values?.dates?.startDate ? [values.dates] : [],
+            },
+          })
+        }
+      >
         {({ handleSubmit, isSubmitting, values }: FormikProps<any>) => (
           <Form onSubmit={handleSubmit}>
             <GridRow container spacing={4}>
-
+              <Grid item xs={6} md={3}>
+                <StyledFormControl>
+                  <StyledLabel htmlFor="locationName">
+                    Location Name
+                  </StyledLabel>
+                  <AutocompleteInput
+                    fieldName="locationName"
+                    options={locations}
+                  />
+                  <ErrorMessage name="locationName" />
+                </StyledFormControl>
+              </Grid>
+            </GridRow>
+            <GridRow container spacing={4}>
               <Grid item xs={6} md={3}>
                 <BookingsRange
                   text={
                     values.dates.startDate && values.dates.endDate
-                      ? `${moment(values.dates.startDate).format(dateFormat)} to ${moment(values.dates.endDate).format(dateFormat)}`
-                      : `${moment().format(dateFormat)} to ${moment().add(30, 'days').format(dateFormat)}`
+                      ? `${moment(values.dates.startDate).format(
+                          dateFormat
+                        )} to ${moment(values.dates.endDate).format(
+                          dateFormat
+                        )}`
+                      : `${moment().format(dateFormat)} to ${moment()
+                          .add(30, 'days')
+                          .format(dateFormat)}`
                   }
                 />
-                <ErrorMessage name='dates' render={msg => <div>{msg}</div>} />
+                <ErrorMessage name="dates" render={(msg) => <div>{msg}</div>} />
               </Grid>
 
               <Grid item xs={6} md={3}>
                 <StyledFormControl>
-                  <StyledLabel htmlFor='interpreter'>
-                    Interpreter
-                    </StyledLabel>
-                  <Field name='interpreter'>
+                  <StyledLabel htmlFor="interpreter">Interpreter</StyledLabel>
+                  <Field name="interpreter">
                     {({ field, form, ...props }: any) => (
                       <StyledTextField
-                        id='interpreter'
-                        variant='outlined'
-                        size='small'
+                        id="interpreter"
+                        variant="outlined"
+                        size="small"
                         {...field}
                         {...props}
                       />
                     )}
                   </Field>
-                  <ErrorMessage name='interpreter' />
+                  <ErrorMessage name="interpreter" />
                 </StyledFormControl>
               </Grid>
 
               <Grid item xs={6} md={3}>
                 <StyledFormControl>
-                  <StyledLabel htmlFor='file'>
-                    Case Number
-                    </StyledLabel>
-                  <Field name='file'>
+                  <StyledLabel htmlFor="file">Case Number</StyledLabel>
+                  <Field name="file">
                     {({ field, form, ...props }: any) => (
                       <StyledTextField
-                        id='file'
-                        variant='outlined'
-                        size='small'
+                        id="file"
+                        variant="outlined"
+                        size="small"
                         {...field}
                         {...props}
                       />
                     )}
                   </Field>
-                  <ErrorMessage name='file' />
+                  <ErrorMessage name="file" />
                 </StyledFormControl>
               </Grid>
-
             </GridRow>
             <GridRow container spacing={4}>
               <Grid item xs={12} md={3}>
                 <StyledFormControl>
-                  <StyledFormLabel htmlFor='submit' />
+                  <StyledFormLabel htmlFor="submit" />
                   <ButtonPrimary
-                    type='submit'
-                    variant='contained'
-                    color='secondary'
+                    type="submit"
+                    variant="contained"
+                    color="secondary"
                     disabled={isSubmitting}
                   >
                     Search
@@ -112,5 +140,5 @@ export default function Search({ getSearchResults }: { getSearchResults: Functio
         )}
       </Formik>
     </Box>
-  )
+  );
 }
