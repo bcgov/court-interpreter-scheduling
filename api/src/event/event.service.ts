13beb97f8a9111ffd58e5904b7a5000dc6d350e8
Michel Alexander <michel@freshworks.io>
2021-04-29 15:24:04 -0700

    basic create functions in the event service

--- api/src/event/event.service.ts
+++ api/src/event/event.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { InterpreterEventEntity } from 'src/event/entities/interpreter-event.entity';
+import { BookingEventEntity } from 'src/event/entities/booking-event.entity';
 
 @Injectable()
-export class EventService {}
+export class EventService {
+  constructor(
+    @InjectRepository(InterpreterEventEntity)
+    private readonly interpreterEventRepository: Repository<InterpreterEventEntity>,
+    @InjectRepository(BookingEventEntity)
+    private readonly bookingEventRepository: Repository<BookingEventEntity>,
+  ) {}
+
+  async createInterpreterEvent({ interpreterId, field, previous, updated }) {
+    const e = this.interpreterEventRepository.create({
+      field,
+      previous,
+      updated,
+      interpreter: interpreterId,
+    });
+    return e;
+  }
+  
+  async createBookingEvent({ bookingId, field, previous, updated}) {
+    const e = this.bookingEventRepository.create({
+      field,
+      previous,
+      updated,
+      booking: bookingId,
+    });
+    return e;
+  }
+}
