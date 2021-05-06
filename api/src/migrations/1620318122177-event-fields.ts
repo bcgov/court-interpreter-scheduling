27f303d18d158c60bf7a7ccab91ed92c9f8f67b2
fwpushan <pushan@freshworks.io>
2021-05-07 00:11:48 +0530

    Add field and other columns to booking_event

--- api/src/migrations/1620318122177-event-fields.ts
+++ api/src/migrations/1620318122177-event-fields.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class EventFields1620318122177 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
@@ -6,7 +6,15 @@ export class EventFields1620318122177 implements MigrationInterface {
       `ALTER TABLE interpreter_event ADD COLUMN IF NOT EXISTS field character varying;
       ALTER TABLE interpreter_event ADD COLUMN IF NOT EXISTS subfield character varying;
       ALTER TABLE interpreter_event ADD COLUMN IF NOT EXISTS language character varying;
-    `);
+    `,
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE booking_event ADD COLUMN IF NOT EXISTS field character varying;
+      ALTER TABLE booking_event ADD COLUMN IF NOT EXISTS subfield character varying;
+      ALTER TABLE booking_event ADD COLUMN IF NOT EXISTS language character varying;
+    `,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
@@ -15,5 +23,11 @@ export class EventFields1620318122177 implements MigrationInterface {
       ALTER TABLE interpreter_event DROP COLUMN IF EXISTS subfield;
       ALTER TABLE interpreter_event DROP COLUMN IF EXISTS language;
     `);
+
+    await queryRunner.query(`
+      ALTER TABLE booking_event DROP COLUMN IF EXISTS field;
+      ALTER TABLE booking_event DROP COLUMN IF EXISTS subfield;
+      ALTER TABLE booking_event DROP COLUMN IF EXISTS language;
+    `);
   }
-};
+}
