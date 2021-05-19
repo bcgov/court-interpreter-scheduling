9e93f0523e2cde236f9394fd8f192ddaad8d8561
fwpushan <pushan@freshworks.io>
2021-05-20 00:45:10 +0530

    [frontend] 1. Remove try/catch and some if block to improve code readability

--- frontend/src/hooks/useError.ts
+++ frontend/src/hooks/useError.ts
@@ -11,41 +11,33 @@ export default function useError({
 }) {
   const { updateErrorContext } = useContext(ErrorContext);
   useEffect(() => {
-    if (error) {
-      let message = error.message || 'An unknown error occurred.';
-      try {
-        // Decode error response
-        if (error.response && error.response.data) {
-          const data = error.response.data;
-          if (data.error) {
-            if (data.error.constructor === Array) {
-              const errors: string[] = data.error || [];
-              message = errors.join('/');
-            } else {
-              message = `${error}`;
-            }
+    let message = error?.message || 'An unknown error occurred.';
+    // Decode error response
+    if (error?.response && error?.response.data) {
+      const data = error.response.data;
+      if (data.error) {
+        if (data.error.constructor === Array) {
+          const errors: string[] = data.error || [];
+          message = errors.join('/');
+        } else {
+          message = `${error}`;
+        }
+      } else {
+        const dataMessage = data.message;
+        if (dataMessage) {
+          if (dataMessage.constructor === Array) {
+            message = dataMessage.join('/');
           } else {
-            const dataMessage = data.message;
-            if (dataMessage) {
-              if (dataMessage.constructor === Array) {
-                message = dataMessage.join('/');
-              } else {
-                message = dataMessage;
-              }
-            }
+            message = dataMessage;
           }
         }
-
-        const errorMessage = `${message} (${error.response?.statusText})`;
-        const finalMessage = prefix
-          ? `${prefix} Error: ${errorMessage}`
-          : `Unable to perform operation. Error: ${errorMessage}`;
-        updateErrorContext(finalMessage);
-      } catch (excp) {
-        const finalMessage = prefix ? `${prefix} ${message}` : message;
-        updateErrorContext(finalMessage);
       }
     }
+
+    const errorMessage = `${message} (${error?.response?.statusText})`;
+    const finalMessage = prefix
+      ? `${prefix} Error: ${errorMessage}`
+      : `Unable to perform operation. Error: ${errorMessage}`;
+    updateErrorContext(finalMessage);
   }, [error, prefix, updateErrorContext]);
-  return;
 }
