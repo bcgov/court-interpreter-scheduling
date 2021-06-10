b218b2a3dcd8edbb8135001be1dc717b6b1b8d23
Jerry <jerryappleid761208@gmail.com>
2021-06-10 12:29:48 -0700

    fix calendar lang and distance view

--- frontend/src/components/calendar/DirectoryCalendar.tsx
+++ frontend/src/components/calendar/DirectoryCalendar.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import moment from 'moment'
-import { Box, Grid, Typography, withStyles } from '@material-ui/core'
-import SearchContext from 'contexts/SearchContext'
-import Month from 'components/calendar/Month'
-import MoreDetails from 'components/calendar/MoreDetails'
-import BookingButton from 'components/table/BookingButton'
+import React from 'react';
+import moment from 'moment';
+import { Box, Grid, Typography, withStyles } from '@material-ui/core';
+import SearchContext from 'contexts/SearchContext';
+import Month from 'components/calendar/Month';
+import MoreDetails from 'components/calendar/MoreDetails';
+import BookingButton from 'components/table/BookingButton';
+import { Language } from 'constants/interfaces';
 
 type CalendarProps = {
   interpreters: Array<any>;
   setInterpreter: Function;
-}
+};
 
 const OutlinedGridItem = withStyles({
   root: {
@@ -25,7 +26,7 @@ const OutlinedGridItem = withStyles({
       borderRight: 'solid 1px #DFECFF',
     },
   },
-})(Grid)
+})(Grid);
 
 const FlexBox = withStyles({
   root: {
@@ -33,72 +34,113 @@ const FlexBox = withStyles({
     flexDirection: 'row',
     alignItems: 'center',
   },
-})(Box)
+})(Box);
 
-export default function Calendar ({ interpreters, setInterpreter }: CalendarProps) {
+export default function Calendar({
+  interpreters,
+  setInterpreter,
+}: CalendarProps) {
   return (
     <SearchContext.Consumer>
       {({ search }) => (
         <Box>
           <Grid container>
             <Grid item xs={3}>
-              <Typography variant='subtitle1'>Interpreters ({interpreters.length} results)</Typography>
+              <Typography variant="subtitle1">
+                Interpreters ({interpreters.length} results)
+              </Typography>
             </Grid>
             <Grid item xs={3}>
-              <Typography variant='subtitle1'>{moment().subtract(1, 'month').format('MMMM YYYY')}</Typography>
+              <Typography variant="subtitle1">
+                {moment().subtract(1, 'month').format('MMMM YYYY')}
+              </Typography>
             </Grid>
             <Grid item xs={3}>
-              <Typography variant='subtitle1'>{moment().format('MMMM YYYY')}</Typography>
+              <Typography variant="subtitle1">
+                {moment().format('MMMM YYYY')}
+              </Typography>
             </Grid>
             <Grid item xs={3}>
-              <Typography variant='subtitle1'>{moment().add(1, 'month').format('MMMM YYYY')}</Typography>
+              <Typography variant="subtitle1">
+                {moment().add(1, 'month').format('MMMM YYYY')}
+              </Typography>
             </Grid>
           </Grid>
-          {
-            interpreters.map(i => (
+          {interpreters.map((i) => {
+            const langs = search.language
+              ? i?.languages.filter(
+                  (l: Language) => l.languageName === search.language
+                )
+              : i?.languages;
+            return (
               <Box mb={1} mt={1}>
                 <Grid container>
                   <OutlinedGridItem item xs={3}>
                     <Box mb={1}>
-                      <Typography variant='subtitle1'>{i.firstName} {i.lastName}</Typography>
+                      <Typography variant="subtitle1">
+                        {i.firstName} {i.lastName}
+                      </Typography>
                     </Box>
                     <Grid container>
-                      <Grid item xs={6}><Typography variant='subtitle1'>Level:</Typography></Grid>
-                      <Grid item xs={6}>Lv 1 - {search.language || 'Portuguese'}</Grid>
-                      <Grid item xs={6}><Typography variant='subtitle1'>Distance:</Typography></Grid>
-                      <Grid item xs={6}>0.5km</Grid>
-                      <Grid item xs={6}><Typography variant='subtitle1'>Drive time:</Typography></Grid>
-                      <Grid item xs={6}>20mins</Grid>
+                      <Grid item xs={6}>
+                        <Typography variant="subtitle1">Level:</Typography>
+                      </Grid>
+
+                      <Grid item xs={6}>
+                        <Grid container justify="flex-start" direction="column">
+                          {langs.map((l: Language) => (
+                            <Grid
+                              item
+                              xs={12}
+                            >{`Lv ${l?.level} - ${l?.languageName}`}</Grid>
+                          ))}
+                        </Grid>
+                      </Grid>
+
+                      {(i?.distance || i?.distance === 0) && (
+                        <>
+                          <Grid item xs={6}>
+                            <Typography variant="subtitle1">
+                              Distance:
+                            </Typography>
+                          </Grid>
+                          <Grid item xs={6}>
+                            {`${i.distance} km`}
+                          </Grid>
+                        </>
+                      )}
                     </Grid>
                     <FlexBox mt={1}>
-                      <BookingButton conflicts={i.conflicts} onClick={() => setInterpreter(i)}>Book</BookingButton>
-                      <Box px={1}><MoreDetails search={search} interpreter={i} setInterpreter={setInterpreter} /></Box>
+                      <BookingButton
+                        conflicts={i.conflicts}
+                        onClick={() => setInterpreter(i)}
+                      >
+                        Book
+                      </BookingButton>
+                      <Box px={1}>
+                        <MoreDetails
+                          search={search}
+                          interpreter={i}
+                          setInterpreter={setInterpreter}
+                        />
+                      </Box>
                     </FlexBox>
                   </OutlinedGridItem>
                   <OutlinedGridItem item xs={3}>
-                    <Month
-                      start={0}
-                      bookings={i?.bookings}
-                    />
+                    <Month start={0} bookings={i?.bookings} />
                   </OutlinedGridItem>
                   <OutlinedGridItem item xs={3}>
-                    <Month
-                      start={1}
-                      bookings={i?.bookings}
-                    />
+                    <Month start={1} bookings={i?.bookings} />
                   </OutlinedGridItem>
                   <OutlinedGridItem item xs={3}>
-                    <Month
-                      start={2}
-                      bookings={i?.bookings}
-                    />
+                    <Month start={2} bookings={i?.bookings} />
                   </OutlinedGridItem>
                 </Grid>
               </Box>
-            ))
-          }
+            );
+          })}
         </Box>
       )}
     </SearchContext.Consumer>
-  )
+  );
 }
