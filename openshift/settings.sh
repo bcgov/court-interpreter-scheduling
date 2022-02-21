e09ff1aa47f730ba393f20f1ebffe8f6c228d1eb
marzmehr <marzmehr@uvic.ca>
2022-02-20 20:55:37 -0800

    minor fix

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,6 +1,6 @@
 export PROJECT_NAMESPACE="1a7b16"
 export GIT_URI="https://github.com/bcgov/court-interpreter-scheduling.git"
-export GIT_REF="dev"
+export GIT_REF="main"
 
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
